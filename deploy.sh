d63dfa7bc835284d6b2836c70811daf92f5d0e2a
novaeric <novaeric@novaericui-MacBookAir.local>
2026-03-05 15:23:42 +0900

    feat: blog setup

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# 서브모듈 최신화
+git submodule update --remote
+
+# Hugo 빌드
+hugo -t Book --gc
+
+# source 브랜치에 빌드 결과 push
+cd public
+git add .
+
+msg="rebuild: $(date +"%Y-%m-%dT%H:%M:%S%z")"
+if [ $# -eq 1 ]; then
+  msg="$1"
+fi
+git commit -m "$msg"
+git push origin source
+
+# main 브랜치에도 반영
+cd ..
+git add .
+if [ $# -eq 1 ]; then
+  msg="$1"
+fi
+git commit -m "$msg"
+git push origin main
